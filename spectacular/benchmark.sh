337a33c853d920a40017d84d83e7b5deb7972040
Matthías Páll Gissurarson <mpg@mpg.is>
2022-09-21 13:42:19 +0200

    Explore bigger expressions in benchmark

--- spectacular/benchmark.sh
+++ spectacular/benchmark.sh
@@ -1,21 +1,21 @@
 #/usr/bin/env bash
 FORMAT="%C %E %M"
-
-
+# RTS="+RTS -N -RTS"
+RTS=""
 for bench in Lists Octonions Regex ListMonad HugeLists; do
     echo "Building $bench..."
-    BRES=$(cabal build && cabal exec -- ghc benchmarks/$bench.hs -O3 -o $bench)
+    BRES=$(cabal build && cabal exec -- ghc benchmarks/$bench.hs -O2 -o $bench)
 done
 
-for p in {1..4}; do
+for p in {2..4}; do
     echo "Timing phase $p..."
     for bench in Lists Octonions Regex ListMonad; do
-        RES=$(/usr/bin/time --output=timed --format="$FORMAT" ./$bench 7 $p | tail -n 1)
+        RES=$(/usr/bin/time --output=timed --format="$FORMAT" ./$bench 7 $p $RTS | tail -n 1)
         TIMERES=$(cat timed)
         echo "$TIMERES $RES"
     done
-    for s in 3 4; do
-        RES=$(/usr/bin/time --output=timed --format="$FORMAT" ./HugeLists $s $p | tail -n 1)
+    for s in 3 4 5 6 7; do
+        RES=$(/usr/bin/time --output=timed --format="$FORMAT" ./HugeLists $s $p $RTS | tail -n 1)
         TIMERES=$(cat timed)
         echo "$TIMERES $RES"
     done
